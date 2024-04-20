2298feaec29839a431bfe515f5ba6f616faf8f76
gab462 <gustavoaraujob1+1@gmail.com>
2024-04-20 11:52:57 -0300

    Simplify conditionals on ttt.fun:with

--- src/fun.lisp
+++ src/fun.lisp
@@ -8,11 +8,9 @@
   (if (numberp k)
       (append (subseq xs 0 k) (list v) (subseq xs (+ k 1)))
       (let ((pos (position k xs)))
-        (cond
-          ((numberp pos)
-           (append (subseq xs 0 pos) (list k v) (subseq xs (+ pos 2))))
-          ((eq pos nil)
-           (append xs (list k v)))))))
+        (if (numberp pos)
+          (append (subseq xs 0 pos) (list k v) (subseq xs (+ pos 2)))
+          (append xs (list k v))))))
 
 (defun update (xs k f)
   (with xs k (funcall f (getf xs k))))
