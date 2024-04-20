6c1e9c20bc298da74775366341a00362ed4ca3f2
gab462 <gustavoaraujob1+1@gmail.com>
2024-04-20 11:42:10 -0300

    Use subseq instead of take & drop

--- src/fun.lisp
+++ src/fun.lisp
@@ -1,24 +1,16 @@
 (in-package :cl-user)
 (defpackage ttt.fun
   (:use :cl)
-  (:export :drop :take :with :update))
+  (:export :with :update))
 (in-package :ttt.fun)
-
-(defun drop (n xs)
-  (if (= n 0) xs
-      (drop (- n 1) (cdr xs))))
-
-(defun take (n xs &optional (acc '()))
-  (if (= n 0) acc
-      (take (- n 1) (cdr xs) (append acc (list (car xs))))))
 
 (defun with (xs k v)
   (if (numberp k)
-      (append (take k xs) (list v) (drop (+ k 1) xs))
+      (append (subseq xs 0 k) (list v) (subseq xs (+ k 1)))
       (let ((pos (position k xs)))
         (cond
           ((numberp pos)
-           (append (take pos xs) (list k v) (drop (+ pos 2) xs)))
+           (append (subseq xs 0 pos) (list k v) (subseq xs (+ pos 2))))
           ((eq pos nil)
            (append xs (list k v)))))))
 
